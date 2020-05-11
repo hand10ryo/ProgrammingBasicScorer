3d9aff41d0f9529c4148e62d05c3509612d0f214
Ryo Matsui <hand10ryo@yahoo.co.jp>
2020-05-11 23:26:47 +0900

    add docstring for CopyChecker

--- CopyChecker.py
+++ CopyChecker.py
@@ -7,7 +7,16 @@ import pandas as pd
 
 
 def similarity_cal(code_str1,code_str2):
-    #delete indent tab space zenkakuspace kaigyou
+    """
+    レーベンシュタイン距離に基づく類似度の計算
+    
+    - input
+    code_str1：【文字列】スクリプトファイル1の全体
+    code_str2：【文字列】スクリプトファイル2の全体
+    
+    - output
+    simirality：【float】類似度
+    """
     table = str.maketrans({
         '\u3000': '',
         ' ': '',
@@ -27,6 +36,7 @@ def copy_checker(path):
     path:【文字列】ワイルドカードを使って該当するチェックしたいファイルを全て指定する
     
     - output
+    df_similarity：【pandas.DataFrame】１列目にファイル１のパス、2列目にファイル2のパス、3列目に類似度となるDF
     
     """
     py_list = glob.glob(path)
@@ -52,11 +62,15 @@ def copy_checker(path):
 
 
 def get_ID(x):
+    "パスから学籍番号を取り出す関数(apply用)"
+    
     year = int(x.split("/")[-1][:2])
     idx = int(x.split("/")[-1][3:8])
     return year*100000 + idx
 
 def get_df_with_ID(df,test=True):
+    "学籍番号の小さい方と大きい方を区別するための関数"
+    
     df_copy = df.copy()
     df_copy["ID1"] = df_copy["file1"].map(get_ID)
     df_copy["ID2"] = df_copy["file2"].map(get_ID)
@@ -65,6 +79,14 @@ def get_df_with_ID(df,test=True):
     return df_copy
 
 def copy_checker_by_filename(filename, test=False):
+    """上のcopycheckをファイル名を基準に呼び出す関数
+    
+    - input 
+    filename：【文字列】ファイル名(19B0000_***.pyの***の部分のみ)
+    
+    - output
+    df.pivot：【pandas.DataFrame】学籍番号を行および列にもち、要素に類似度を示す上三角行列
+    """
     if test:
         path = os.getcwd() + f"/testsubmit/*{filename}.py"
     else:
